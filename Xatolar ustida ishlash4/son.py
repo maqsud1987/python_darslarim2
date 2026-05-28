60727b8188d77cac15f7ea071d4a7560668f576a
maqsud1987 <maqsudsulaymonov86@gmail.com>
2026-05-28 16:57:43 +0500

    Create son.py

---
+++ Xatolar ustida ishlash4/son.py
@@ -0,0 +1,12 @@
+def juft_mi(n):
+    return n%2 == 0
+
+
+# def kvadrat_yuza(a):
+#     return a + a        # ← xato! to'g'risi a*a bo'lishi kerak
+
+# def uchburchak_yuza(asos, balandlik):
+#     return asos * balandlik   # ← xato! to'g'risi (asos*balandlik)/2
+
+# def doira_yuza(r):
+#     return 3.14159 * r * r    # ← bu to'g'ri!
